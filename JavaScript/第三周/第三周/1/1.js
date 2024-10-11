af74f06889482a98bb69fd403936accc70c68b7e
yifengdadas <yifengdadas@gmail.com>
2024-10-11 20:23:33 +0800

    Delete JavaScript/第三周 directory

--- JavaScript/第三周/第三周/1/1.js
+++
@@ -1,2 +0,0 @@
-let name = prompt('请输入姓名', '翔安');
-alert(`你输入的名字是：`+name);
--- JavaScript/第三周/第三周/3/3.js
+++
@@ -1,6 +0,0 @@
-let input_string = prompt("1");
-if (Number(input_string) == 1) {
-    alert("yes");
-}else {
-    alert("no");
-}
